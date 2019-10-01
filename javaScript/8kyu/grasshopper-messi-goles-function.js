12b70b6f44a21c76d29beae97caff1a2b472e48f
NeerajKumar22 <ineerajkr@gmail.com>
2019-10-01 15:32:14 +0530

    grasshopper-messi-goles-function solution

---
+++ javaScript/8kyu/grasshopper-messi-goles-function.js
@@ -0,0 +1,14 @@
+// Messy goals function
+// Messy is a soccer player with goals in three leagues:
+
+// LaLigan
+// Copa del Rey
+// Champions.
+// Complete the goals function to return his total goals (the sum) for all three leagues.
+
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+console.log(goals(43, 10, 5));
